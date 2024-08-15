511911c5a43c1700039da45114afc6c92f632f12
MiguelR222 <miguel.ruiz0652@gmail.com>
2024-08-15 15:36:23 -0600

    svg arrow in send button

--- pages/index.js
+++ pages/index.js
@@ -93,7 +93,9 @@ export default function Chat() {
           onClick={handleSend}
           className="ml-2 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
         >
-          Send
+          <svg data-name="1-Arrow Up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-6 h-6">
+            <path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"/>
+          </svg>
         </button>
       </div>
     </div>
