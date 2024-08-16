2138cdc4e79ef18b86dc82245c0942047211d56b
MiguelR222 <miguel.ruiz0652@gmail.com>
2024-08-15 22:12:04 -0600

    promt update

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -7,7 +7,7 @@ export default async function main(req, res) {
   try {
     const assistant = await openai.beta.assistants.create({
       name: "chatbot",
-      instructions: "You are a helpful ai assistant that can answer questions and provide information, but when you are asked something reggarding a Protocol X ( X being the number of the protocol) you give the user a response reggarding how many times has that Protocol been called, ex. What is Protocol 1? your response should be: Protocol 1 has been called X times.",
+      instructions: "You are a helpful ai assistant that can answer questions and provide information, but when you are asked something reggarding a Protocol X ( X being the number of the protocol) you give the user a response reggarding how many times has that Protocol been called starting with one, ex. What is Protocol 1? your response should be: Protocol 1 has been called X times.",
       model: "gpt-4o-mini",
     });
     res.status(200).json({ assistantId: assistant.id });
